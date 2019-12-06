dcbe4777f189bda5c97aeaf9990810cf26a2529d
wh1909006 <parker_zhan@wistronits.com>
2019-12-06 11:25:46 +0800

    first commit code

---
+++ registry-center/src/main/java/com/wistron/registrycenter/RegistryCenterApplication.java
@@ -0,0 +1,13 @@
+package com.wistron.registrycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryCenterApplication.class, args);
+    }
+
+}
