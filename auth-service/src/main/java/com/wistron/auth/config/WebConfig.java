1a70f8475e23e8894a1616e9b90e7ef44ced3880
superYouth <690386530@qq.com>
2019-12-10 22:14:17 +0800

    update

---
+++ auth-service/src/main/java/com/wistron/auth/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.wistron.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/login");
+        registry.addViewController("/login").setViewName("my-login");
+
+    }
+}
--- auth-service/src/main/java/com/wistron/auth/config/WebSecurityConfig.java
+++ auth-service/src/main/java/com/wistron/auth/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/userlogin","/userlogout","/mylogin");
+        web.ignoring().antMatchers("/userlogin","/userlogout","/login");
 
     }
 //    @Bean
@@ -36,11 +36,11 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
 
         http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
-//                .loginPage("/mylogin")           // 设置登录页面
-//                .loginProcessingUrl("/mylogin")  // 自定义的登录接口
+                .loginPage("/login")           // 设置登录页面
+//                .loginProcessingUrl("/login")  // 自定义的登录接口
                 .and()
                 .authorizeRequests()
-                .antMatchers("/userlogin","/userlogout","/mylogin") // 定义哪些URL需要被保护、哪些不需要被保护
+                .antMatchers("/userlogin","/userlogout","/login") // 定义哪些URL需要被保护、哪些不需要被保护
                 .permitAll()     // 设置所有人都可以访问登录页面
                 .anyRequest()               // 任何请求,登录后可以访问
                 .authenticated()
